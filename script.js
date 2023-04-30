7de9005520ba036c0af2bcd5bef149260d0d1ec0
Aamir Hannan <aamirhannan08@gmail.com>
2023-04-30 23:39:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let currentPlayer = 'X';
+let arr = Array(9).fill(null);
+
+function chackWinner(){
+    if(
+        //row winner
+        (arr[0] != null && arr[0] == arr[1] && arr[1] == arr[2])||
+        (arr[3] != null && arr[3] == arr[4] && arr[4] == arr[5])||
+        (arr[6] != null && arr[6] == arr[7] && arr[7] == arr[8])||
+        //col winner
+        (arr[0] != null && arr[0] == arr[3] && arr[3] == arr[6])||
+        (arr[1] != null && arr[1] == arr[4] && arr[4] == arr[7])||
+        (arr[2] != null && arr[2] == arr[5] && arr[5] == arr[8])||
+        //diagonal
+        (arr[0] != null && arr[0] == arr[4] && arr[4] == arr[8])||
+        (arr[2] != null && arr[2] == arr[4] && arr[4] == arr[6])
+    )
+    {
+        document.write(`winner is ${currentPlayer}`);
+        return;
+    }
+
+    if(!arr.some((e) => e === null)){
+        document.write(`Draw!!`);
+        return;
+    }
+}
+
+function handleClick(el){
+    const id = Number(el.id);
+    if(arr[id] != null)  return;
+    arr[id] = currentPlayer;
+    el.innerText = currentPlayer;
+    chackWinner();
+    currentPlayer = currentPlayer == "X" ? "0" : "X";
+}
